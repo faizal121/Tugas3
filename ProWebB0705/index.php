62f3f841da584ad42660d00d7132702f019db367
faizal121 <37105108+faizal121@users.noreply.github.com>
2018-03-15 20:30:56 +0700

    Update index.php

--- ProWebB0705/index.php
+++ ProWebB0705/index.php
@@ -1,57 +1,55 @@
+<!DOCTYPE html>
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>SIGN IN</title>
-        <link rel="icon" href="asset/images/favicon.ico">
-        <link rel="stylesheet" href="asset/css/bootstrap.css">
-        <link rel="stylesheet" href="asset/css/floating-labels.css">
-        
-        <script src="asset/js/jquery.min.js"></script>
-        <script src="asset/js/popper.min.js"></script>
-        <script src="asset/js/bootstrap.min.js"></script>
+        <title>SIM COBA</title>
+        <link rel="icon" href="assets/images/favicon.ico">
+        <link rel="stylesheet" href="assets/CSS/bootstrap.css">
+        <link rel="stylesheet" href="assets/CSS/floating-labels.css">
         <script>
-            window.setTimeout(function () {
-                $(".alert").fadeOut(400,0).slideUp(400,function (){
-                $(this).remove();
-            });
-            },5000);
+            function hellocoba(){
+                //var email = document.getElementById("email").value;
+                //alert("Email Anda : "+email);
+                document.getElementById("email").addEventListener("keyup", function(event){
+                    event.preventDefault();
+                    if(event === 13){
+                        document.getElementById("password").focus();
+                    }
+                });
+            }
+            
+            function hellocoba2(){
+                alert("Password Lupa Ya..");
+            }
         </script>
     </head>
     <body>
-        <div class="alert alert-danger fade show" role="alert">
-            IHIRRRRRR !!!!!
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <button type="button" class="btn btn-outline-success" data-toggle="modal"
-            data-target="#DialogModalKu">
-                    Tampilkan Dialog !
-        </button>
-        
-        <div class="modal fade" id="DialogModalKu" tabindex="-1" role="dialog"
-            aria-labelledby="DialogModalKu" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="DialogModalLabel">
-                        Ini Modal !
-                    </h5>
-                    <button type="button" class="close" data-dismiss="modal"
-                        aria-label="close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    Bagian Modal Body nya . . .
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary"
-                        data-dismiss="modal">Tutup
-                    </button>
-                </div>
-                </div>
-                </div>
-        </div>
+        <form class="form-signin">
+            <div class="text-center mb-4">
+                <img class="mb-4" src="assets/images/bootstrap-solid.svg" 
+                     style="width: 72px; height: auto;">
+                <h1>Login Form</h1>
+                <p>Login Sekarang Juga Yukkk....</p>
+            </div>
+            <div class="form-label-group">
+                <input onkeyup="hellocoba();" class="form-control" type="email" name="email" id="email" 
+                       placeholder="email" required="yes" autofocus="yes"
+                       autocomplete="no" maxlength="30">
+                <label for="email">Email Address</label>
+            </div>
+            <div class="form-label-group">
+                <input class="form-control" type="password" name="password" id="password" 
+                       placeholder="password" required="yes" maxlength="8">
+                <label for="password">Password</label>
+            </div>
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox"> Remember Me
+                </label>
+            </div>
+            <button class="btn btn-lg btn-success btn-block" type="button">Sign In</button>
+            <button onclick="hellocoba2();"class="btn btn-lg btn-primary btn-block" type="button">Forget Password</button>
+        </form>
     </body>
 </html>
